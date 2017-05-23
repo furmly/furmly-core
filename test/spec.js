e30939827d987b98568efb4148741960448380f7
chidi onuekwusi <chidionuekwusi@gmail.com>
2017-05-23 11:52:03 +0100

    First commit

---
+++ test/spec.js
@@ -0,0 +1,11 @@
+var sinon = require('sinon'),
+	model = require('../src/index.js');
+
+describe('A process must be uniquely identifiable system wide', function() {
+
+
+});
+
+describe('There can be several steps in a process but there must be atleast one step', function() {
+
+});
